8e5da042c63106e75cb4bfd269cfbf4216175e86
Ashish Nath <107504126+Ashish98th@users.noreply.github.com>
2023-04-11 13:59:38 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter,Link,Route,Routes,useLocation} from "react-router-dom";
+import {Route,Routes,useLocation} from "react-router-dom";
 import './App.scss';
 import Particles from "react-tsparticles";  
 import {loadFull} from "tsparticles";
